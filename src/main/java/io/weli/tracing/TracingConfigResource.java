c2b1150081d3644dd726664e74ad2fd651c328d0
Weinan Li <l.weinan@gmail.com>
2018-05-17 20:33:56 +0800

    .

--- src/main/java/io/weli/tracing/TracingConfigResource.java
+++ src/main/java/io/weli/tracing/TracingConfigResource.java
@@ -1,33 +1,35 @@
 package io.weli.tracing;
 
+import org.jboss.resteasy.spi.HttpRequest;
+import org.jboss.resteasy.spi.ResteasyDeployment;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.jboss.resteasy.tracing.RESTEasyTracingLogger;
+import org.jboss.resteasy.tracing.RESTEasyTracingUtils;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 
 @Path("/")
 public class TracingConfigResource extends Application {
 
     @GET
     @Path("/type")
-    public String type() {
-        return ResteasyProviderFactory.getTracingType().toString();
+    public String type(@Context ResteasyDeployment deployment) {
+        return RESTEasyTracingUtils.getTracingConfig(deployment.getProviderFactory()).toString();
     }
 
     @GET
     @Path("/level")
-    public String level() {
-        return ResteasyProviderFactory.getTracingThreshold().toString();
+    public String level(@Context ResteasyDeployment deployment) {
+        return RESTEasyTracingUtils.getTracingThreshold(deployment.getProviderFactory()).toString();
     }
 
     @GET
     @Path("/logger")
-    public String logger() throws NoSuchMethodException {
-        RESTEasyTracingLogger logger = ResteasyProviderFactory.getTracingLogger();
-//        logger.log(RESTEasyServerTracingEvent.METHOD_INVOKE, TracingConfigResource.class.toString(),
-//                "logger()");
+    public String logger(@Context HttpRequest request) throws NoSuchMethodException {
+        RESTEasyTracingLogger logger = RESTEasyTracingLogger.getInstance(request);
         return logger.toString();
     }
 
