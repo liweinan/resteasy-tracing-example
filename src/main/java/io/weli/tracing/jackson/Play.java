d8542036fd0de25cac6db9525de9b263013df161
Weinan Li <l.weinan@gmail.com>
2018-08-02 03:32:22 +0800

    .

---
+++ src/main/java/io/weli/tracing/jackson/Play.java
@@ -0,0 +1,22 @@
+package io.weli.tracing.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+
+public class Play {
+    public static void main(String[] args) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Car car = new Car("yellow", "renault");
+        objectMapper.writeValue(System.out, car);
+
+        Class mapperClass = Class.forName("com.fasterxml.jackson.databind.ObjectMapper");
+        Object mapper = mapperClass.newInstance();
+        Method method = mapperClass.getDeclaredMethod("writeValueAsString", Object.class);
+        method.setAccessible(true);
+        System.out.println(method.invoke(mapper, car));
+
+
+    }
+}
---
+++ src/main/java/io/weli/tracing/jsonb/Play2.java
@@ -0,0 +1,15 @@
+package io.weli.tracing.jsonb;
+
+import java.lang.reflect.Method;
+
+public class Play2 {
+    private static Object mapper = null;
+    private static Class mapperClass = null;
+
+    public static void main(String[] args) throws Exception {
+        mapperClass = Class.forName("javax.json.bind.JsonbBuilder");
+        Method method = mapperClass.getDeclaredMethod("create");
+        mapper = method.invoke(null);
+        System.out.println(mapper);
+    }
+}
